01f7be0acd358864c56488034d20b001a8876a77
margheritamatteinipalmerini <62596331+margheritamatteinipalmerini@users.noreply.github.com>
2020-06-01 11:09:34 +0200

    Update R_code_exame.r

--- R_code_exame.r
+++ R_code_exame.r
@@ -121,148 +121,140 @@ plot(cadmium,copper,pch=17,col="pink",main="Primo plot",xlab="cadmio",ylab="copp
 
 ############ R spatial: scopriamo le funzioni spaziali di R 
 
-
+install.packages("GGally")
 install.packages("sp")  
-#chiamiamo un paccheto dall'esterno, quindi virgolette
-#finestre con luoghi: tutti luoghi i cui viene sviluppato R al momento (CRAN => CRAN MIRROR): meraviglioso!
+# MMP chiamiamo un paccheto dall'esterno, quindi virgolette
+# MMP finestre con luoghi: tutti luoghi i cui viene sviluppato R al momento (CRAN => CRAN MIRROR): meraviglioso!
 library(sp) 
-# serve per spiegare al software quale pacchetto richiamare
+# MMP serve per spiegare al software quale pacchetto richiamare
 data(meuse)
-#data: funzione per i dati
-#meuse: dataset
+# MMP data: funzione per i dati
+# MMP meuse: dataset
 head(meuse)
-#in questo modo vediamo direttamente non solo gli elementi ma anche le coordinate che utilizzeremo ora
-#esercizi: plot di rapporto fra cadmio e piombo
-#alla fine faremo un multipannello in cui compaiono più plot
-#alleghiamo il dataframe 
+#MMP in questo modo vediamo direttamente non solo gli elementi ma anche le coordinate che utilizzeremo ora
+# esercizi: plot di rapporto fra cadmio e piombo
+# MMP alla fine faremo un multipannello in cui compaiono più plot
+# MMP alleghiamo il dataframe 
 attach(meuse)
-plot(cadmium,lead,col="red",pch=19,cex=2)
-#virgola: separator
-#col: cambio colore dei punti come più mi piace
-#dentro R la funzione per cambiare carattere è pch
-#caratter exateration... aumentare (>1) diminuire (<1)
-#dovrebbe uscire un grafico
-#exercise o esercizio: è un esercizio da fare
-#exercise: plot di copper e zinco con  caratteree (simbolo) trnagolo e colore verde
-#triangolo pch=17
+plot(cadmium,lead,col="purple",pch=19,cex=2)
+#MMP virgola: separatore
+#MMP col: cambio colore dei punti come più mi piace
+#MMP dentro R la funzione per cambiare carattere è pch
+#MMP carattere exateration... aumentare (>1) diminuire (<1)
+#MMP dovrebbe uscire un grafico
+#MMP exercise: plot di copper e zinco con  caratteree (simbolo) trinagolo e colore verde
+#MMPtriangolo pch=17
 plot(copper,zinc,col="green",pch=17,cex=2)
-#2: quindi il doppio 
-#solitamnete le vergolette si mettono su un testo: avviso che ci troviamo di fronte ad un testo, ma anche quando facciamo il nostro software con qualcosa di esterno.
-#xlab: cambio etichetta
-plot(copper,zinc,col="green",pch=17,cex=2,xlab="rsme",ylab"zinco")
-#multiframe (mf) o multipanel: funzione importantissima per mettere più di un grafico all'interno di una finestra
-#par: funzioe per tutti i pannelli, quindi anche tutti i grafici. numeri di righe e colonne
-par(mfrow=c(1,2))
-#numeri in fila: vettori
-#prima bisogna metterci le cose dentro prima di copiare ed incollare su R
+#MMP 2: quindi il doppio 
+#MMP solitamnete le vergolette si mettono su un testo: avviso che ci troviamo di fronte ad un testo, ma anche quando facciamo dialogare  il nostro software con qualcosa di esterno.
+#MMP xlab: cambio etichetta
+plot(copper,zinc,col="green",pch=17,cex=2,xlab="rame",ylab"zinco")
+#MMP multiframe (mf) o multipanel: funzione importantissima per mettere più di un grafico all'interno di una finestra
+#MMP par: funzioe per tutti i pannelli, quindi anche tutti i grafici. numeri di righe e colonne
+#MMP numeri in fila: vettori, c davanti
 par(mfrow=c(1,2))
 plot(cadmium,lead,col="red",pch=19,cex=2)
 plot(copper,zinc,col="green",pch=17,cex=2)
-#copiare tutto il blocco (le 3 righe sopra) , incollare e premere invio
-#invertiamo riga/colonna colonna/riga
+# MMP copiare tutto il blocco (le 3 righe sopra) , incollare in R e premere invio
+# MMP invertiamo riga/colonna colonna/riga
 par(mfrow=c(2,1))
 plot(cadmium,lead,col="red",pch=19,cex=2)
 plot(copper,zinc,col="green",pch=17,cex=2)
-#multipannelo con pairs --> esiste un pacchetto che può aiuare
-#multiframe automatico: andiamo ad installare 
-install.packages("GGally")
-#virgolette perchè dall'esterno
-library(GGally)
-#richiamiamolo
-#pairs:multipannello di multivariabili
-ggpairs(meuse)
-#ci vorrà un sacco, subset []
+#MMP multipannelo con pairs --> esiste un pacchetto che può aiuare
+#MMP multiframe automatico: andiamo ad installare 
+install.packages("GGally") ##MMP tutti i pacchetti da installare per rieguire un codice andrebbero messi tutti nelle prime righe
+#MMP virgolette perchè dall'esterno
+library(GGally) #MMP richiamiamolo
+#MMP pairs:multipannello di multivariabili
+ggpairs(meuse) #MMP ci vorrà un sacco, subset []
 ggpairs(meuse[,3:6])
-# , vuol dire "partiamo da"
-# : "fino a"
-#grafico: che cos'è? sulla parte diagonale vediamo le singole varibiali e la distribuzione dei dati
-#plot variabili in relaione fra di loro (in modo simetrico)
-#correlazioni 
-#coeficente fra -1 e 1
+# MMP, vuol dire "partiamo da"
+#MMP  : "fino a"
+#gMMP rafico: che cos'è? sulla parte diagonale vediamo le singole varibiali e la distribuzione dei dati
+#MMP plot variabili in relaione fra di loro (in modo simetrico)
+# MMP correlazioni
+#MMP coeficente fra -1 e 1
 
-#spatial!
+####MMP  spatial!
 
 head(meuse)
 
-#coordinate: x e y come in tutti i grafici
-#diciamolo al software che ci sono delle coordinate
+#MMP coordinate: x e y come in tutti i grafici
+#MMP diciamo al software che ci sono delle coordinate:
 coordinates(meuse)
-#non incolliamola subito su R: è particolare
+### MMP non incolliamola subito su R: è particolare
 coordinates(meuse)=~x+y
-#in R,per raggruppare dei pezzi usiamo la tilde (Linux:Alt Gr + ì - Windows: Alt + 126) 
-#adesso possiamo incollarlo su R
+#MMP in R,per raggruppare dei pezzi usiamo la tilde
+# MMP adesso possiamo incollarlo su R
 plot(meuse)
-#grafico
+#MMPg rafico
 spplot(meuse,"zinc")
-#funzione per plottare i dati spazialmente
-#all'interno della funzion spplot le variabili vanno scitte con le virgolette perchè vengono prese dall'esterno
-#il nostro primo grafico spaziale: che emozione!
+#MMP funzione per plottare i dati spazialmente
+#MMP all'interno della funzion spplot le variabili vanno scitte con le virgolette perchè vengono prese dall'esterno
+#MMP il nostro primo grafico spaziale: che emozione!
 
-# finiamo domani!
-
-### parte due (giorno seguente)
-#richiaiamo la libreria sp e i dati da usare
+################### PARTE DUE
+#MMP  richiaiamo la libreria sp e i dati da usare
 library(sp)
 data(meuse)
-# coordinate xy, ascisse e ordinate, del dataset
-# head serve per dare un occhiata veloce al dataset! (memo!)
+# MMP coordinate xy, ascisse e ordinate, del dataset
+# MMPhead serve per dare un occhiata veloce al dataset! (memo!)
 coordinates(meuse)=~x+y
-#quando si inserisce una funzione su R e non vediamo risposta, vuol dire che tutto è andato bene!
+#MMP ATTENZIONE: quando si inserisce una funzione su R e non vediamo risposta, vuol dire che tutto è andato bene!
 spplot(meuse,"zinc")
-#sp (ilpacchetto) + plot! in questa funzione le variabili vanno indicate fra virgolette
-#deve uscire una immagine 
+#MMP sp (il pacchetto) + plot  in questa funzione le variabili vanno indicate fra virgolette
+#MMP deve uscire una immagine 
 
-#esercizio: spplot dei dati di rame
-#per vedere come si chiama il rame o head(meuse) : xy in coordinate 
+#esercizio: spplot dei dati del rame
+#MMP per vedere come si chiama il rame: head(meuse) : xy in coordinate 
 head(meuse)
-##oppure: per vedere SOLO i nomi delle colonne
+#MMP oppure: per vedere SOLO i nomi delle colonne
 names(meuse)
-#quindi:
+#MMP quindi:
 spplot(meuse,"copper")
-# avevamo visto che erano coordinati rame e zinco, infatti anche il primo ha valori decrescenti in base alla lontananza dal fiume
+#MMP avevamo visto che erano coordinati rame e zinco, infatti anche il primo presenta valori decrescenti in base alla lontananza dal fiume
 
-#stesso plottaggio,altra funzione di sp : bubble
+#MMP stesso plottaggio,altra funzione di sp : bubble
 bubble(meuse,"zinc")
-#grafico: stessa rapresentazione di prima, ma invece che dei colori che si relazionano ai diversi valori, qui vengonorelazionati a delle bolle "bubble" in base ad alte o basse concentraione
-# conferma le concentrazioni dell'altro grafico
-## questo mi sembra più immediato ed elegante. si chiama infografico
+#MMP grafico: stessa rapresentazione di prima, ma invece che dei colori che si relazionano ai diversi valori, qui vengono relazionati a delle bolle (bubble) in base ad alte o basse concentraione
+# MMP conferma le concentrazioni dell'altro grafico
+# MMP questo tipo digrafico, infografico, forse èpiù immediato ed elegante
 
-#esercizio: funzione bubble del rame ma colorato di rosso
-##se si cerca la funzione in rete, vengono fornite tutte le informazioni del caso
+#MMP esercizio: funzione bubble del rame ma colorato di rosso
 bubble(meuse,"copper",col="red")
-## red va scitto fra virgolette perchè fa riferimento a valori numerici che indicano come viene fatto il colore rosso
+#MMP red va scitto fra virgolette perchè fa riferimento a valori numerici che indicano come viene fatto il colore rosso
 
-#esempio: ci inventiamo dei dati
+#MMP esempio con dati inventati
 # foraminiferi (Sofia), carbon capture (Marco)
 10, 20, 35, 55, 67, 80
-#in R, questa serie di numeri è un vettore (array)
+#MMP in R, questa serie di numeri è un vettore (array)
 foram <- c(10, 20, 35, 55, 67, 80)
-#abbiamo dato un nome al dataset!abbiamo creato un oggetto
+#MMP abbiamo dato un nome al dataset : abbiamo creato un oggetto
 carbon <- c(5, 15, 30, 70, 85, 99)
-# abbiamo dato nome anche all'altra serie di numeri (di Marco)
-#inseriamoli su R
-#plottiamo i due dati per vedere se sono relazionati fra loro
+#MMP abbiamo dato nome anche all'altra serie di numeri (di Marco)
+# MMP inseriamoli su R
+#MMP plottiamo i due dati per vedere se sono relazionati fra loro
 plot(foram, carbon, col="green", cex=2, pch=19)
-#stretta relazione fra il carbonio intrappolato dai foraminiferi e la densità di questi ultimi
-##piccolo esercizio per creare oggeti
+#MMP stretta relazione fra il carbonio intrappolato dai foraminiferi e la densità di questi ultimi
 
-#scarica tabella da iol, covid_agg.csv
-#crea una nuova cartellahome: percorso più corto che si può fare!! "lab"
 
-#dati dall'esterno: covid19
-#specifcare la cartella al software con cui utilizziamo
-#cartella appena create 
-#setwd("~/lab")
+#### MMP scarica tabella da iol, covid_agg.csv
+#MMP crea una nuova cartella in home: percorso più corto che si può fare!! "lab"
+
+# MMP dati dall'esterno: covid19
+#MMP specifcare la cartella al software con cui utilizziamo
+#MMP cartella appena create 
+#MMP setwd("~/lab")
 setwd("~/lab")
 covid <- read.table("covid_agg.csv",head=TRUE)
-#agg : dati aggregati
-#dobbiamo spiegare a r che ci sono dei titoli
-#TRUE: variabili 1-0, vero falso, .poteva essere anche solo T
-#relazionato al nome covid
+#MMP agg : dati aggregati
+#MMP dobbiamo spiegare a r che ci sono dei titoli
+#MMP TRUE: variabili 1-0, vero falso, .poteva essere anche solo T
+#MMP relazionato al nome covid
 head(covid)
-#vedi la tabellina?
 
-## continua nella prossima puntata...
+
+
 
 
 

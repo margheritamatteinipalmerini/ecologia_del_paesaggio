0d7931d6dcd303e08c871c8d35ae05bf28b24f9a
margheritamatteinipalmerini <62596331+margheritamatteinipalmerini@users.noreply.github.com>
2020-06-04 10:51:52 +0200

    Update R_code_exame.r

--- R_code_exame.r
+++ R_code_exame.r
@@ -940,6 +940,130 @@ grafico1<-ggplot(output,aes(x=cover,y=before,color=cover))+geom_bar(stat="identi
 grafico22<-ggplot(output,aes(x=cover,y=after,color=cover))+geom_bar(stat="identity",fill="white")+ylim(0,100)
 grid.arrange(grafico1,grafico2,nrow=1)
 
+#############################################################################################################################################################################################
+#############################################################################################################################à
+###############################################################################################################
+###############################################################################################################
+###################################################################################################################
+### OTTAVO CODICE
+### R MULTITEMP NO2!
+## ANALISI NO2 DA DATI ESA, GENNAIO-MARZO 2020
+setwd("~/lab/")
+
+# MMP Librerie che ci servono
+library(raster)
+#MMP importiamo le singole immagini scaricate e nella cartella lab
+
+
+EN01 <- raster("EN_00001.png")
+plot(EN01)
+dev.off()
+
+# MMP  importare tutte le immagii
+EN02 <- raster("EN_0002.png")
+EN03 <- raster("EN_0003.png")
+EN04 <- raster("EN_0004.png")
+EN05 <- raster("EN_0005.png")
+EN06 <- raster("EN_0006.png")
+EN07 <- raster("EN_0007.png")
+EN08 <- raster("EN_0008.png")
+EN09 <- raster("EN_0009.png")
+EN10 <- raster("EN_0010.png")
+EN11 <- raster("EN_0011.png")
+EN12 <- raster("EN_0012.png")
+EN13 <- raster("EN_0013.png")
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100) 
+cl <- colorRampPalette(c('red','orange','yellow'))(100) # 
+plot(EN01, col=cl)  #MMP  inizio gennaio
+plot(EN13, col=cl) # MMP marzo
+
+# MMP  operiamo una vera e propria statistica
+
+    par(mfrow=c(1,2))
+plot(EN01, col=cl)
+plot(EN13, col=cl)
+difno2 <- EN13 - EN01
+cldif <- colorRampPalette(c('blue','black','yellow'))(100) #
+plot(difno2, col=cldif)
+ 
+ #MMP  metodo più lungo
+ 
+par(mfrow=c(4,4))
+plot(EN01, col=cl)
+plot(EN02, col=cl)
+plot(EN03, col=cl)
+plot(EN04, col=cl)
+plot(EN05, col=cl)
+plot(EN06, col=cl)
+plot(EN07, col=cl)
+plot(EN08, col=cl)
+plot(EN09, col=cl)
+plot(EN10, col=cl)
+plot(EN11, col=cl)
+plot(EN12, col=cl)
+plot(EN13, col=cl)
+
+dev.off()
+
+
+### PARTE DUE!!
+
+
+setwd("~/lab/")
+load("EN.RData")
+ls()
+
+library(raster)# MMP  per importare più immagini insieme 
+## MMP  per importare più immagini insieme bisogna creare una cartella contenente tutti le immagini EN e selezionare questa cartella come nuova WD
+setwd("~/lab/esa_no2")
+
+#  MMP creiamo una rlist contenente la lista di file .png
+rlist<-list.files(pattern=".png")
+rlist # MMP  visualiziamo il contenuto del nuovo data creato
+
+# MMP save raster into list
+# MMP  lapply applica una funzione su una lista o un vettore (serie di elementi). 
+# MMP  In questo caso la funzione che vogliamo applicare per caricare i singoli dati sono brick (immagine satellitare con tutti i sensori) e raster (immagine con un solo sensore)
+#  MMP  applichiamo alla listra rlis la funzione raster
+listafinale<-lapply(rlist, raster)
+
+listafinale 
+# MMP  visualizziamo i RasterLayer 
+
+#  MMP  funzione stack permette di impacchettare tutte le immagini in una unica
+EN <- stack(listafinale)
+
+cl<-colorRampPalette(c('red','orange','yellow'))(100)
+# MMP  possiamo dunque plottare l'immagine
+plot(EN,col=cl)
+
+
+####
+
+library(raster)
+setwd("~/lab/esa_no2")
+
+rlist
+listafinale<-lapply(rlist,raster)
+listafinale
+EN<-stack(listafinale)
+
+# MMP  una volta caricate le immagini creiamo una differenza tra l'ultima immagine (EN 13 corrispondente a Marzo) e la prima immagine (EN 01 corrispondente a Gennaio) 
+difEN<- EN$EN_0013-EN$EN_0001
+
+# MMPcolorRampPalette
+cld<-colorRampPalette(c('blue','white','red'))(100)
+
+#  MMP  plottiamo la differenza delle immagini con il colore creato così da visualizzare le differenze tra Marzo e Gennaio
+plot(difEN,col=cld)
+
+# MMP  boxplot
+boxplot(EN,horizontal=T,outiline=F,axes=T)
+#  MMP horizontal=T -> disposizione orizzontale
+#  MMP outline=F -> senza linee esterne
+# MMP  axes=T di defoult, mette gli assi (il contrario axes=F li toglie)
+
 ###################################################################################################################
 #####################################################################################################################
 ######################################## #########################################################################
